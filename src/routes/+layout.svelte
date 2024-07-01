5dab5ef7d38173c2a754d8436fd087fad8e3d134
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-07-01 19:11:01 +0330

    initial landing page styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,42 @@
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
+<script>
+	import "../app.css";
+	import Icon from '@iconify/svelte';
+	
+</script>
+
+<style lang="postcss">
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+  :global(body) {
+		color: #B6C4B6;
+    background-color: #222831;
+		font-family: "Noto Sans", sans-serif;
+		padding: 0 1rem;
+  }
+</style>
+
+<nav class="flex items-center justify-between max-w-4xl  mx-auto py-4">
+	<div>
+		<h1 class="text-xl font-bold">CloudBuddy</h1>
+	</div>
+	<div>
+		<a href="/login" class="group">
+			<Icon icon="mdi:user" class="text-3xl group-hover:opacity-80" />
+		</a>
+	</div>
 </nav>
 
-<slot></slot>
+<div style="height: calc(100vh - 4rem - 4rem)">
+	<slot />
+</div>
 
-<style>
-
-	body {
-		background-color: #176B87;
-	}
-
-</style>
+<footer class="flex justify-center items-center p-4 ">
+	<a href="/github">
+		<Icon icon="mdi:github" class="text-xl inline mb-1" />
+	</a>
+	<span class="mb-1">&nbsp;|&nbsp;</span>
+	<p>
+		<span>Made with lots of&nbsp;</span>
+		<span><Icon icon="bi:clouds-fill" class="text-xl inline mb-1" /></span>
+	</p>
+</footer>
