e4617b2ab83c15bed465eaa09935042607eed37f
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-07-01 15:53:33 +0330

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
+
+<style>
+
+	body {
+		background-color: #176B87;
+	}
+
+</style>
