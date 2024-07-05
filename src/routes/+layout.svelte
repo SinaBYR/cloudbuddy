bd55cb535801e067c31b8cba725a008c631d0fa7
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-07-05 13:30:44 +0330

    fix: update layout height and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,19 +3,19 @@
 	import Icon from '@iconify/svelte';
 	
 </script>
-
+<!-- color: #B6C4B6; -->
 <style lang="postcss">
 	@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
 
   :global(body) {
-		color: #B6C4B6;
+		@apply text-slate-300;
     background-color: #222831;
 		font-family: "Noto Sans", sans-serif;
 		padding: 0 1rem;
   }
 </style>
 
-<nav class="flex items-center justify-between max-w-4xl  mx-auto py-4">
+<nav class="flex items-center justify-between max-w-4xl mx-auto py-4">
 	<div>
 		<h1 class="text-xl font-bold">CloudBuddy</h1>
 	</div>
@@ -26,15 +26,19 @@
 	</div>
 </nav>
 
-<div style="height: calc(100vh - 4rem - 4rem)">
+<!-- style="height: calc(100vh - 4rem - 4rem)" -->
+<div class="max-w-4xl mx-auto py-4 mb-20">
 	<slot />
 </div>
 
-<footer class="flex justify-center items-center p-4 ">
-	<a href="/github">
+<footer
+	class="fixed bottom-0 left-0 w-full flex justify-center items-center p-4 bg-[#222831]"
+	style="box-shadow: 0 -1px 32px 12px #222831;"
+>
+	<a href="https://github.com/sinabyr/cloudbuddy">
 		<Icon icon="mdi:github" class="text-xl inline mb-1" />
 	</a>
-	<span class="mb-1">&nbsp;|&nbsp;</span>
+	<span class="mb-1 select-none">&nbsp;|&nbsp;</span>
 	<p>
 		<span>Made with lots of&nbsp;</span>
 		<span><Icon icon="bi:clouds-fill" class="text-xl inline mb-1" /></span>
