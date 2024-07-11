72dd057fca6095d70850242057d66de911d1c762
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-07-11 19:14:48 +0330

    feat(routes): login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,86 @@
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+	import { Button, ButtonGroup, Input, InputAddon, Label } from "flowbite-svelte";
+	import { onMount } from "svelte";
+	import { fade } from "svelte/transition";
+	import { type AuthStore, authStore } from "../../stores/authStore";
+	import { parseJwtPayload } from "$lib/jwt";
+	import { isLoggedIn } from "$lib/auth";
+	import { goto } from "$app/navigation";
+
+	let username = '', passphrase = '', login = false
+	$: valid = username.length !== 0 && passphrase.length >= 8
+
+	onMount(() => {
+		if(isLoggedIn()) goto('/')
+	})
+
+	async function handleSumbit() {
+		const res = await fetch("http://localhost:8080/v1/auth/signin", {
+			method: 'POST',
+			body: JSON.stringify({ username, passphrase }),
+		})
+		const { token, ...user }: AuthStore = await res.json()
+		localStorage.setItem('user', JSON.stringify(user))
+		localStorage.setItem('token', token)
+		authStore.set({token, ...user})
+		goto('/dashboard')
+	}
+
+
+function handle() {
+	const payload = parseJwtPayload(localStorage.getItem('token')!)
+	console.log(login)
+	console.log(payload)
+}
+</script>
+
+<button on:click={() => handle()}>Clock</button>
+
+<form
+	in:fade
+	class="max-w-xl mx-auto mt-16"
+	on:submit|preventDefault={handleSumbit}
+>
+	<div class="mb-6">
+		<Label for="username" class="block mb-2">Username</Label>
+		<ButtonGroup class="w-full">
+			<InputAddon>
+				<Icon icon="mdi:user" class="w-5 h-5" />
+			</InputAddon>
+			<Input
+				id="username"
+				type="text"
+				bind:value={username}
+			/>
+		</ButtonGroup>
+	</div>
+
+	<div class="mb-6">
+		<Label for="passphrase" class="block mb-2">Passphrase</Label>
+		<ButtonGroup class="w-full">
+			<InputAddon>
+				<Icon icon="mdi:key" class="w-5 h-5" />
+			</InputAddon>
+			<Input
+				id="passphrase"
+				type="password"
+				bind:value={passphrase}
+			/>
+		</ButtonGroup>
+	</div>
+
+	<div>
+		<Button
+			type="submit"
+			color="alternative"
+			class="w-full text-light-secondary dark:text-dark-secondary"
+			disabled={!valid}
+		>Login</Button>
+	</div>
+</form>
+
