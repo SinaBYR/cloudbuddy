3061684db1739faea2a057f5c474e8628b6fb5b5
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-07-18 17:00:26 +0330

    fix(routes): fetch current user images only

--- src/routes/manage/+page.ts
+++ src/routes/manage/+page.ts
@@ -1,12 +1,17 @@
-import type { DataStore } from "../../stores/dataStore";
-import { manageStore } from "../../stores/manageStore";
+import { manageStore } from "../../stores/manageStore"
+import type { DataStore } from "../../stores/dataStore"
+import { getLocalAuth } from "$lib/auth"
 
 export const ssr = false
 export const csr = true
 
 export const load = async () => {
+	const user = getLocalAuth()
+	if(!user) return
+	const params = new URLSearchParams()
+	params.set('userId', user.uuid)
 	try {
-		const res = await fetch('http://localhost:8080/v1/images')
+		const res = await fetch('http://localhost:8080/v1/images?' + params.toString())
 		const data: DataStore = await res.json()
 
 		manageStore.set(data)
